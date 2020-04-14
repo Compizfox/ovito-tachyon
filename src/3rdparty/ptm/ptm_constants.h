d854367bae76f079545f65afd01c866f305fc8fa
pmla <peter.mahler.larsen@gmail.com>
2020-04-14 12:01:01 +0200

    size_t needs to include <cstddef>

--- src/3rdparty/ptm/ptm_constants.h
+++ src/3rdparty/ptm/ptm_constants.h
@@ -12,6 +12,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #include <cmath>
 #include <cstdint>
+#include <cstddef>
 
 //------------------------------------
 //    definitions
