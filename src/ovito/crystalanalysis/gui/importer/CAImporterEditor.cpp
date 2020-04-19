5c1772482f1ca1bcde3168a7f2d056babdd56e8d
Alexander Stukowski <stukowski@mm.tu-darmstadt.de>
2020-04-19 09:51:39 +0200

    Attempt to work around GCC 4.7 compiler bug leading to error in LAMMPSTextDumpImporterEditor.cpp

--- src/ovito/crystalanalysis/gui/importer/CAImporterEditor.cpp
+++ src/ovito/crystalanalysis/gui/importer/CAImporterEditor.cpp
@@ -47,7 +47,7 @@ void CAImporterEditor::createUI(const RolloutInsertionParameters& rolloutParams)
 	BooleanParameterUI* multitimestepUI = new BooleanParameterUI(this, PROPERTY_FIELD(FileSourceImporter::isMultiTimestepFile));
 	// The following signal handler updates the parameter UI whenever the isMultiTimestepFile parameter of the current file source importer changes.
 	// It is needed, because target-changed messages are surpressed for this property field and the normal update mechanism for the parameter UI doesn't work.
-	connect(this, &PropertiesEditor::contentsReplaced, this, [con = QMetaObject::Connection(), multitimestepUI](RefTarget* editObject) mutable {
+	connect(this, &PropertiesEditor::contentsReplaced, this, [con = QMetaObject::Connection(), multitimestepUI = multitimestepUI](RefTarget* editObject) mutable {
 		disconnect(con);
 		con = editObject ? connect(static_object_cast<FileSourceImporter>(editObject), &FileSourceImporter::isMultiTimestepFileChanged, multitimestepUI, &ParameterUI::updateUI) : QMetaObject::Connection();
 	});
