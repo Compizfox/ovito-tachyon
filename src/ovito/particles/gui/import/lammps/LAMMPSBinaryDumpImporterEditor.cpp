721d602ed6a9526ede101b569e1dd55a832aa4a3
Alexander Stukowski <stukowski@mm.tu-darmstadt.de>
2020-04-19 09:22:47 +0200

    Fixed linking error on Linux due to recent commit

--- src/ovito/particles/gui/import/lammps/LAMMPSBinaryDumpImporterEditor.cpp
+++ src/ovito/particles/gui/import/lammps/LAMMPSBinaryDumpImporterEditor.cpp
@@ -123,7 +123,7 @@ void LAMMPSBinaryDumpImporterEditor::createUI(const RolloutInsertionParameters& 
 	// The following signal handler updates the parameter UI whenever the isMultiTimestepFile parameter of the current file source importer changes.
 	// It is needed, because target-changed messages are surpressed for this property field and the normal update mechanism for the parameter UI doesn't work.
 	connect(this, &PropertiesEditor::contentsReplaced, this, [con = QMetaObject::Connection(), multitimestepUI](RefTarget* editObject) mutable {
-		QObject::disconnect(con);
+		disconnect(con);
 		con = editObject ? connect(static_object_cast<FileSourceImporter>(editObject), &FileSourceImporter::isMultiTimestepFileChanged, multitimestepUI, &ParameterUI::updateUI) : QMetaObject::Connection();
 	});
 	sublayout->addWidget(multitimestepUI->checkBox());
